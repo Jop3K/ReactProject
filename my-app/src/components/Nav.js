67b07b819f8ba72ce84362850e1a3554c29d8452
Joni Halonen <jonpethal@gmail.com>
2019-04-01 17:10:04 +0300

    Viikko 2 commit

--- my-app/src/components/Nav.js
+++ my-app/src/components/Nav.js
@@ -1,18 +1,37 @@
-import React from 'react';
+import React from 'react'
+import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
 
-const Nav = () => {
+const Nav = (props) => {
   return (
     <nav>
       <ul>
         <li>
-          Home
+          <Link to='/home'>Home</Link>
         </li>
+        {props.checkLogin() &&
+        <React.Fragment>
+          <li>
+            <Link to='/profile'>Profile</Link>
+          </li>
+          <li>
+            <Link to='/logout'>Logout</Link>
+          </li>
+        </React.Fragment>
+        }
+        {!props.checkLogin() &&
         <li>
-          Profile
+          <Link to='/'>Login</Link>
         </li>
+        }
+
       </ul>
     </nav>
-  );
-};
+  )
+}
 
-export default Nav;
+Nav.propTypes = {
+  checkLogin: PropTypes.func
+}
+
+export default Nav
